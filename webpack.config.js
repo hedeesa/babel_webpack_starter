1c9e80ebf96c8facac8e7fac26a4f7def20bc262
Hedeesa <40231268+hedeesa@users.noreply.github.com>
2019-09-01 17:36:33 +0430

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,29 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   entry: {
-    app: [
-      '@babel/polyfill',
-      './src/app.js',
-    ],
+    app: ["@babel/polyfill", "./src/app.js"]
   },
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, "build"),
+    filename: "app.bundle.js"
   },
   module: {
-    rules: [{
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-           presets: ['@babel/preset-env', '@babel/preset-stage-0']
-        }
-    }]
+    // rules: [{
+    //     test: /\.js?$/,
+    //     exclude: /node_modules/,
+    //     loader: 'babel-loader',
+    //     query: {
+    //        presets: ['@babel/preset-env', '@babel/preset-stage-0']
+    //     }
+    // }]
+    rules: [
+      {
+        test: /\.js$/, // include .js files
+        enforce: "pre", // preload the jshint loader
+        exclude: /node_modules/, // exclude any and all files in the node_modules folder
+        loader: "babel-loader"
+      }
+    ]
   }
-}
+};
